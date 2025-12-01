fa5c49a30439d74f0eddab661a6dd68651848c8f
Soumya Hegde <soumyahegde826@gmail.com>
2025-12-01 12:56:32 +0530

    Reverse a String-input=soumya, output=aymuos

---
+++ Level1/Stringgreverse.java
@@ -0,0 +1,12 @@
+// import java.util.Scanner;
+public class Stringgreverse{
+    public static void main(String[] args) {
+        // Scanner input = new Scanner(System.in);
+        // TODO: your code here
+        String str="soumya";
+        for(int i=str.length()-1;i>=0;i--){
+            System.out.print(str.charAt(i));
+        }
+
+    }
+}
