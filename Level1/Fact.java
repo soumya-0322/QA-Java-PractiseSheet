ec5a203100c862029789b6a4ea6ec956d0f59bac
Soumya Hegde <soumyahegde826@gmail.com>
2025-12-16 22:21:56 +0530

    finding the factorial of the number using recurssion method

---
+++ Level1/Fact.java
@@ -0,0 +1,13 @@
+public class Fact {
+
+    static int factorial(int n){
+        if(n==0){
+           return 1;
+        }
+    return n*factorial(n-1);
+    }
+    public static void main(String[] args) {
+        int n=5;
+       System.out.println("the factorial of "+n+"! is:"+factorial(n));
+    }
+}
