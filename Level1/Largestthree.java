1d6fab2d19e87235facbd41c365d81693c3612f9
Soumya Hegde <soumyahegde826@gmail.com>
2025-12-16 22:26:36 +0530

    finding the largest of three numbers

---
+++ Level1/Largestthree.java
@@ -0,0 +1,21 @@
+public class Largestthree {
+    public static void main(String[] args) {
+        int num1=-52;
+        int num2=33;
+        int num3=33;
+    
+        if(num1>num2 && num1>num3 )
+        {
+            System.out.println(num1+" is greater");
+        }
+        else if(num2>num3)
+            {
+                System.out.println(num2+" is greater");
+            }
+       
+        else
+                System.out.println(num3+" is greater");
+
+        
+    }
+}
